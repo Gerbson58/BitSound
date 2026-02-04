19b2179f1701f1a869642612bb12be416459587b
gerbson58 <gsg1993@gmail.com>
2026-02-04 16:03:17 -0400

    uploads capas

--- script.js
+++ script.js
@@ -1,62 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-
-
-
-
     const artistsData = [
-
         { name: 'Ana Castela', image: './img/anacastela3.jpg' },
         { name: 'Anita', image: './img/anita.jpg' },
-        { name: 'Gustavo Lima', image: './img/gustavolima3.jpg' },
-        { name: 'Bit Sound', image: './img/logo2.jpg' },
-        { name: 'Michael Jackson', image: './img/MichaelJackson.jpg' },
-        { name: 'Taylor Swifty', image: './img/taylorswift.jpg' },
-        { name: 'The Beatles', image: './img/TheBeatles.jpg' }
+        { name: 'Gustavo Lima', image: './img/gusttavolima.jpg' },
+        { name: 'Michael Jackson', image: './img/MichaelJackson1.jpg' },
+        { name: 'Taylor Swift', image: './img/taylorswift.jpg' },
+        { name: 'The Beatles', image: './img/TheBeatles1.jpg' }
     ];
 
     const albumsData = [
-        { name: '0 Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano', image: './img/nacastela3.jpg' },
-        { name: 'Nada como um dia após o outro', artist: 'Racionais', image: './img/nacastela3.jpg' },
-        { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image: './img/nacastela3.jpg' },
-        { name: 'CAJU', artist: 'Liniker', image: './img/nacastela3.jpg' },
-        { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image: './img/nacastela3.jpg' },
-        { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image: './img/nacastela3.jpg' },
-        { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image: './img/nacastela3.jpg' },    
+        { name: 'Boiadeira Internacional', artist: 'Ana Castela', image: './img/albumanacastela.jpg' },
+        { name: 'Bang!', artist: 'Anita', image: './img/bang.jpg' },
+        { name: 'Gusttavo Lima In Grecce', artist: 'Gusttavo Lima', image: './img/gustavolima3.jpg' },
+        { name: 'Thriller', artist: 'Michael Jackson', image: './img/MichaelJackson.jpg' },
+        { name: 'Lover', artist: 'Taylor Swift', image: './img/lover.jpg' },
+        { name: 'Abbey Road', artist: 'The Beatles', image: './img/TheBeatles.jpg' }
     ];
 
-    const artistsGrid = document.querySelector ('.artists-grid')
-    const albumsGrid = document.querySelector ('.albums-grid')
+    const artistsGrid = document.querySelector('.artists-grid');
+    const albumsGrid = document.querySelector('.albums-grid');
 
+    // 🔠 Ordena artistas alfabeticamente
+    artistsData.sort((a, b) => a.name.localeCompare(b.name));
+
+    // 🎨 Renderiza artistas
     artistsData.forEach(artist => {
-        const artistCard = document.createElement('div')
-        artistCard.classList.add('artist-card')
+        const card = document.createElement('div');
+        card.classList.add('artist-card');
+        card.innerHTML = `
+            <img src="${artist.image}" alt="${artist.name}">
+            <div>
+                <h3>${artist.name}</h3>
+            </div>
+        `;
+        artistsGrid.appendChild(card);
+    });
 
-        artistCard.innerHTML = `
-            <img src="${artist.image}" alt="imagem do ${artist.name}">
-                <   div>
-                    <h3>${artist.name}</h3>
-                    <p>${artista}</p>
-                </div>
-            `
-        artistGrid.appendChild(artistCard)
-    })
-                
-
+    // 🎨 Renderiza álbuns
     albumsData.forEach(album => {
-        const albumCard = document.createElement('div')
-        albumCard.classList.add('album-card')
-
-        albumCard.innerHTML = `
-            <img src="${album.image}" alt="imagem do ${album.name}">
+        const card = document.createElement('div');
+        card.classList.add('album-card');
+        card.innerHTML = `
+            <img src="${album.image}" alt="${album.name}">
             <div>
                 <h3>${album.name}</h3>
                 <p>${album.artist}</p>
             </div>
-        `
+        `;
+        albumsGrid.appendChild(card);
+    });
 
-    artistGrid.appendChild(albumCard)
-    })
-
-})
-
+});
